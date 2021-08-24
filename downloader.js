84168797bb5cb12f2054fa63e5a6d8fbebc4321d
Hanan <88724205+Lisajhkjkk@users.noreply.github.com>
2021-08-24 20:57:52 +0530

    Create downloader.js

--- downloader.js
+++ downloader.js
@@ -32,11 +32,11 @@ Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: false, desc: IG_DESC}, async (me
         const msg = `${type}`
 
 	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: "Made By WhatsAsenaPublic"
+          caption: "ɪͥᴛͭsᷤ ᴍͫᴇͤ☚⃟𝐒𝐎𝐍 𝐘𝐄 𝐉𝐈𝐍𝐍👻"
         })}
 		 	 
 	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: "Made By WhatsAsenaPublic"
+          caption: "ɪͥᴛͭsᷤ ᴍͫᴇͤ☚⃟𝐒𝐎𝐍 𝐘𝐄 𝐉𝐈𝐍𝐍👻"
         })}
 	
         
@@ -71,7 +71,7 @@ Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (me
         const msg = `*${CAPTION}*: ${judul}`
 
         await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: "Made By WhatsAsenaPublic"
+          caption: "ɪͥᴛͭsᷤ ᴍͫᴇͤ☚⃟𝐒𝐎𝐍 𝐘𝐄 𝐉𝐈𝐍𝐍👻"
         })
       })
       .catch(
